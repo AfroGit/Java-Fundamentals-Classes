d4ad286e88bee4af552018d214e09e1301d49b40
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-08-24 16:11:01 +0000

    Two of the components of a method declaration comprise the method signature—the method's name and the parameter types.

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-}
+// class Main {
+//   public static void main(String[] args) {
+//     System.out.println("Hello world!");
+//   }
+// }
+
+/*The only required elements of a method declaration are the method's return type, name, a pair of parentheses, (), and a body between braces, {}. 
+
+    1.)Modifiers—such as public, private, 
+    2.)The return type—the data type of the value returned by the method, or void if the method does not return a value.
+    3.)The method name—the rules for field names apply to method names as well, but the convention is a little different.
+    4.)The parameter list in parenthesis—a comma-delimited list of input parameters, preceded by their data types, enclosed by parentheses, (). If there are no parameters, you must use empty parentheses.
+   5.) An exception list—to be discussed later.
+    6.)The method body, enclosed between braces—the method's code, including the declaration of local variables, goes here.
+*/ 
+
+// public double calculateAnswer(double wingSpan, int numberOfEngines,
+//                               double length, double grossTons) {
+    //do the calculation here
+// }
+
+public String madeFirstMethod(int param, String param, boolen param){}
